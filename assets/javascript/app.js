ef1592408871905eb546477cf3cc00b50c0b6216
meowster93 <cmendoza6277@yahoo.com>
2019-03-06 18:31:07 -0500

    still need to add giphy stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,18 @@ $(document).ready(function () {
 // create varaible with an array of topics for giphs
 var topics = ["One Punch Man", "One Piece", "Dragon Ball Super", "Sailor Moon", "Code Geass","Steins;Gate", "Erased","Black Butler","Naruto", "Soul Eater"]
 var APIkey= jWL1Z34W3Q5t4uM0IV3Z5K4TTp0O1yTR;
-var queryURL;
-
-
-
 
 //----------------------------create functions----------------------------------------------//
 
+function displayAnime(){
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + i + "&api_key=jWL1Z34W3Q5t4uM0IV3Z5K4TTp0O1yTR";
+
+var i = $(this).data("search");
+console.log(i);
+}
 
 
-
-
+displayAnime();
 
 
 });
